0e8d7f8e8dde54ce675e07de6106024ac112cf00
MD. AL AMIN SOHAG <267598403+alaminsohagofficial@users.noreply.github.com>
2026-09-04 03:37:54 +0600

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,20 +6,25 @@ const { getCustomers, addCustomer } = require('../controllers/customerController
 const { getSuppliers, addSupplier } = require('../controllers/supplierController');
 const { getPaymentAccounts, updateAccountBalance } = require('../controllers/accountController');
 const { sendCustomSms } = require('../controllers/smsController');
+const { createSale, getSales } = require('../controllers/saleController');
 
-// Customer Routes
+// 1. Customer Management Routes
 router.get('/customers', getCustomers);
 router.post('/customers', addCustomer);
 
-// Supplier Routes
+// 2. Supplier Management Routes
 router.get('/suppliers', getSuppliers);
 router.post('/suppliers', addSupplier);
 
-// Payment Account Routes
+// 3. Payment Account Routes (bKash, Bank, etc.)
 router.get('/payment-accounts', getPaymentAccounts);
 router.put('/payment-accounts/:id', updateAccountBalance);
 
-// Salsabilah SMS Notification Routes
+// 4. Salsabilah SMS Notification Routes
 router.post('/sms/send', sendCustomSms);
+
+// 5. Sales & Invoice Management Routes (Triggers automated SMS)
+router.get('/sales', getSales);
+router.post('/sales', createSale);
 
 module.exports = router;
